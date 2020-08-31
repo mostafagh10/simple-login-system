ab873b1106198f6332d7d0668623af9bc7b4bc51
mostafa ghazaly <ghazalymostafa07@gmail.com>
2020-08-31 22:30:58 +0200

    Updates

---
+++ precache-manifest.396dbccc9b56b800f9509ba26ce6fdb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ab418df4fdcbe17e32fd30a45c416d",
+    "url": "/simple-login-system/index.html"
+  },
+  {
+    "revision": "687cf74dd60f02d8af34",
+    "url": "/simple-login-system/static/css/2.2058ee32.chunk.css"
+  },
+  {
+    "revision": "f013706f3400dab4ad70",
+    "url": "/simple-login-system/static/css/main.b68cc09c.chunk.css"
+  },
+  {
+    "revision": "687cf74dd60f02d8af34",
+    "url": "/simple-login-system/static/js/2.1bfcd8f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-login-system/static/js/2.1bfcd8f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f013706f3400dab4ad70",
+    "url": "/simple-login-system/static/js/main.1de872b3.chunk.js"
+  },
+  {
+    "revision": "a1d8ed04272f79655637",
+    "url": "/simple-login-system/static/js/runtime-main.5ccb854c.js"
+  }
+]);
